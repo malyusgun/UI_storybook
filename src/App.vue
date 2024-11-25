3515a447a10c824e0e91693de7a9b2cf52000d33
Дмитрий Малюгин <d.malygin@iqdev.digital>
2024-11-25 21:26:19 +0500

    feat: install Storybook

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,18 @@
 </script>
 
 <template>
+  <main class="container">
+    Hello, storybook!
+  </main>
 </template>
 
 <style scoped>
+.container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 64px;
+  font-style: italic;
+}
 </style>

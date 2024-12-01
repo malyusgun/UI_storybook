b73591a7e069117e697625ee87149f73011693b8
Дмитрий Малюгин <d.malygin@iqdev.digital>
2024-12-01 21:47:06 +0500

    feat: more icons (102pcs for now) and replace to other folder as 'Mono icons'

---
+++ src/stories/icons/Mono/ArrowLeftIcon.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+interface Props {
+  color?: string;
+  size?: string | number;
+}
+defineProps<Props>();
+</script>
+
+<template>
+  <svg
+    id="left-arrow"
+    :fill="color ?? '#000000'"
+    :width="size ?? '40px'"
+    :height="size ?? '40px'"
+    viewBox="0 0 24 24"
+    data-name="Flat Line"
+    xmlns="http://www.w3.org/2000/svg"
+    class="icon flat-line"
+  >
+    <line
+      id="primary"
+      x1="21"
+      y1="12"
+      x2="3"
+      y2="12"
+      :stroke="color ?? '#000000'"
+      style="fill: none; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2"
+    ></line>
+    <polyline
+      id="primary-2"
+      data-name="primary"
+      points="6 9 3 12 6 15"
+      :stroke="color ?? '#000000'"
+      style="fill: none; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2"
+    ></polyline>
+  </svg>
+</template>
+
+<style scoped></style>

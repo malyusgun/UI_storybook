aed1cb4c30bdd75d250ed33e654d98b03475b654
Дмитрий Малюгин <d.malygin@iqdev.digital>
2024-12-28 15:26:16 +0500

    fix: sort icons

--- src/stories/icons/Mono/SortDownIcon.vue
+++ src/stories/icons/Mono/SortDownIcon.vue
@@ -11,19 +11,20 @@ defineProps<Props>();
     :width="`${size ?? 40}px`"
     :height="`${size ?? 40}px`"
     viewBox="0 0 24 24"
-    fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    aria-labelledby="sortDownIconTitle"
+    :stroke="color ?? '#000000'"
+    stroke-width="2.4"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    fill="none"
+    :color="color ?? '#000000'"
   >
-    <path d="M4 16L13 16" :stroke="color ?? '#000000'" stroke-width="1.5" stroke-linecap="round" />
-    <path d="M6 11H13" :stroke="color ?? '#000000'" stroke-width="1.5" stroke-linecap="round" />
-    <path d="M8 6L13 6" :stroke="color ?? '#000000'" stroke-width="1.5" stroke-linecap="round" />
-    <path
-      d="M17 4L17 20L20 16"
-      :stroke="color ?? '#000000'"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
+    <path d="M11 14H16" />
+    <path d="M11 9H19" />
+    <path d="M11 4H22" />
+    <path d="M9 18L5 22L1 18" />
+    <path d="M5 2V20" />
   </svg>
 </template>
 

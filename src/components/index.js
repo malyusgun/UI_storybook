5d022789fad559fc5488432f6755edb5dfff71ad
Дмитрий Малюгин <d.malygin@iqdev.digital>
2025-02-17 18:30:15 +0500

    fix: try to fix errors with aliases in using as lib

---
+++ src/components/index.js
@@ -0,0 +1,47 @@
+import Button from './Button/Button.vue';
+import Carousel from './Carousel/Carousel.vue';
+import Checkbox from './Checkbox/Checkbox.vue';
+import ColorPicker from './ColorPicker/ColorPicker.vue';
+import Divider from './Divider/Divider.vue';
+import Drawer from './Drawer/Drawer.vue';
+import InputDiv from './InputDiv/InputDiv.vue';
+import Knob from './Knob/Knob.vue';
+import MenuDial from './MenuDial/MenuDial.vue';
+import Modal from './Modal/Modal.vue';
+import Paginator from './Paginator/Paginator.vue';
+import Popup from './Popup/Popup.vue';
+import ProgressBar from './ProgressBar/ProgressBar.vue';
+import Rating from './Rating/Rating.vue';
+import Select from './Select/Select.vue';
+import SelectButton from './SelectButton/SelectButton.vue';
+import Slider from './Slider/Slider.vue';
+import Table from './Table/Table.vue';
+import Tag from './Tag/Tag.vue';
+import Toast from './Toast/Toast.vue';
+import ToggleSwitch from './ToggleSwitch/ToggleSwitch.vue';
+import TreeList from './TreeList/TreeList.vue';
+
+export {
+  Button,
+  Carousel,
+  Checkbox,
+  ColorPicker,
+  Divider,
+  Drawer,
+  InputDiv,
+  Knob,
+  MenuDial,
+  Modal,
+  Paginator,
+  Popup,
+  ProgressBar,
+  Rating,
+  Select,
+  SelectButton,
+  Slider,
+  Table,
+  Tag,
+  Toast,
+  ToggleSwitch,
+  TreeList,
+};

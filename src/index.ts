bbfce72341f88193a1bb1224788af8bf60335747
Дмитрий Малюгин <d.malygin@iqdev.digital>
2025-08-13 15:51:25 +0500

    refactor: make icon easier, change index.js to index.ts and edit vite.config

---
+++ src/index.ts
@@ -0,0 +1,295 @@
+// Mono icons
+import Age18Icon from './icons/Mono/Age18Icon.vue';
+import AlarmIcon from '@icons/Mono/AlarmIcon.vue';
+import AirplaneIcon from '@icons/Mono/AirplaneIcon.vue';
+import ArrowLeftIcon from '@icons/Mono/ArrowLeftIcon.vue';
+import ArrowForwardIcon from '@icons/Mono/ArrowForwardIcon.vue';
+import ArchiveIcon from '@icons/Mono/ArchiveIcon.vue';
+import AnchorLinkIcon from '@icons/Mono/AnchorLinkIcon.vue';
+import AnchorIcon from '@icons/Mono/AnchorIcon.vue';
+import AlignRightIcon from '@icons/Mono/AlignRightIcon.vue';
+import AlignLeftIcon from '@icons/Mono/AlignLeftIcon.vue';
+import AlignCenterIcon from '@icons/Mono/AlignCenterIcon.vue';
+import ArrowRightIcon from '@icons/Mono/ArrowRightIcon.vue';
+import AtIcon from '@icons/Mono/AtIcon.vue';
+import AwardIcon from '@icons/Mono/AwardIcon.vue';
+import BackspaceIcon from '@icons/Mono/BackspaceIcon.vue';
+import BadgeIcon from '@icons/Mono/BadgeIcon.vue';
+import BallFootballIcon from '@icons/Mono/BallFootballIcon.vue';
+import BallIcon from '@icons/Mono/BallIcon.vue';
+import BanknoteIcon from '@icons/Mono/BanknoteIcon.vue';
+import BellIcon from '@icons/Mono/BellIcon.vue';
+import BellOffIcon from '@icons/Mono/BellOffIcon.vue';
+import BoxIcon from '@icons/Mono/BoxIcon.vue';
+import BrightnessIcon from '@icons/Mono/BrightnessIcon.vue';
+import BulbIcon from '@icons/Mono/BulbIcon.vue';
+import CalculatorIcon from '@icons/Mono/CalculatorIcon.vue';
+import CalendarIcon from '@icons/Mono/CalendarIcon.vue';
+import CalendarAddIcon from '@icons/Mono/CalendarAddIcon.vue';
+import CalendarRemoveIcon from '@icons/Mono/CalendarRemoveIcon.vue';
+import CardsIcon from '@icons/Mono/CardsIcon.vue';
+import ChartLineIcon from '@icons/Mono/ChartLineIcon.vue';
+import ChartPieIcon from '@icons/Mono/ChartPieIcon.vue';
+import ChatIcon from '@icons/Mono/ChatIcon.vue';
+import ChatWritingIcon from '@icons/Mono/ChatWritingIcon.vue';
+import CheckMarkIcon from '@icons/Mono/CheckMarkIcon.vue';
+import ChemistryFlaskIcon from '@icons/Mono/ChemistryFlaskIcon.vue';
+import CodeIcon from '@icons/Mono/CodeIcon.vue';
+import CoinsIcon from '@icons/Mono/CoinsIcon.vue';
+import ColorPaletteIcon from '@icons/Mono/ColorPaletteIcon.vue';
+import CompassIcon from '@icons/Mono/CompassIcon.vue';
+import ConstructionWorkerIcon from '@icons/Mono/ConstructionWorkerIcon.vue';
+import ContactsIcon from '@icons/Mono/ContactsIcon.vue';
+import CropIcon from '@icons/Mono/CropIcon.vue';
+import CrossRoundFilledIcon from '@icons/Mono/CrossRoundFilledIcon.vue';
+import CrossIcon from '@icons/Mono/CrossIcon.vue';
+import CubeIcon from '@icons/Mono/CubeIcon.vue';
+import CupIcon from '@icons/Mono/CupIcon.vue';
+import CursorIcon from '@icons/Mono/CursorIcon.vue';
+import LineDashedIcon from '@icons/Mono/LineDashedIcon.vue';
+import LineDiagonalIcon from '@icons/Mono/LineDiagonalIcon.vue';
+import DiamondIcon from '@icons/Mono/DiamondIcon.vue';
+import DiaryIcon from '@icons/Mono/DiaryIcon.vue';
+import DiceIcon from '@icons/Mono/DiceIcon.vue';
+import DigIcon from '@icons/Mono/DigIcon.vue';
+import DislikeIcon from '@icons/Mono/DislikeIcon.vue';
+import DisplayIcon from '@icons/Mono/DisplayIcon.vue';
+import DocumentIcon from '@icons/Mono/DocumentIcon.vue';
+import DollarIcon from '@icons/Mono/DollarIcon.vue';
+import DotsHorizontalIcon from '@icons/Mono/DotsHorizontalIcon.vue';
+import DotsVerticalIcon from '@icons/Mono/DotsVerticalIcon.vue';
+import LineDottedIcon from '@icons/Mono/LineDottedIcon.vue';
+import DownloadIcon from '@icons/Mono/DownloadIcon.vue';
+import DropIcon from '@icons/Mono/DropIcon.vue';
+import DumbbelIcon from '@icons/Mono/DumbbelIcon.vue';
+import EarthIcon from '@icons/Mono/EarthIcon.vue';
+import EditIcon from '@icons/Mono/EditIcon.vue';
+import EyeIcon from '@icons/Mono/EyeIcon.vue';
+import EncyclopediaIcon from '@icons/Mono/EncyclopediaIcon.vue';
+import ExitIcon from '@icons/Mono/ExitIcon.vue';
+import FeedbackIcon from '@icons/Mono/FeedbackIcon.vue';
+import FilterIcon from '@icons/Mono/FilterIcon.vue';
+import FingerprintIcon from '@icons/Mono/FingerprintIcon.vue';
+import FireIcon from '@icons/Mono/FireIcon.vue';
+import FlagIcon from '@icons/Mono/FlagIcon.vue';
+import FlashIcon from '@icons/Mono/FlashIcon.vue';
+import FlashlightIcon from '@icons/Mono/FlashlightIcon.vue';
+import FolderLockIcon from '@icons/Mono/FolderLockIcon.vue';
+import FrameIcon from '@icons/Mono/FrameIcon.vue';
+import FullScreenIcon from '@icons/Mono/FullScreenIcon.vue';
+import GameControllerIcon from '@icons/Mono/GameControllerIcon.vue';
+import GiftIcon from '@icons/Mono/GiftIcon.vue';
+import GlassesIcon from '@icons/Mono/GlassesIcon.vue';
+import HamburgerIcon from '@icons/Mono/HamburgerIcon.vue';
+import HandIcon from '@icons/Mono/HandIcon.vue';
+import HomeIcon from '@icons/Mono/HomeIcon.vue';
+import ImageEditIcon from '@icons/Mono/ImageEditIcon.vue';
+import ImageIcon from '@icons/Mono/ImageIcon.vue';
+import MoveIcon from '@icons/Mono/MoveIcon.vue';
+import DocumentAddIcon from '@icons/Mono/DocumentAddIcon.vue';
+import DocumentDeleteIcon from '@icons/Mono/DocumentDeleteIcon.vue';
+import DocumentEditIcon from '@icons/Mono/DocumentEditIcon.vue';
+import ParagraphIcon from '@icons/Mono/ParagraphIcon.vue';
+import PhoneHandsetIcon from '@icons/Mono/PhoneHandsetIcon.vue';
+import PlusCircleIcon from '@icons/Mono/PlusCircleIcon.vue';
+import PlusIcon from '@icons/Mono/PlusIcon.vue';
+import PointerIcon from '@icons/Mono/PointerIcon.vue';
+import SaveIcon from '@icons/Mono/SaveIcon.vue';
+import SearchIcon from '@icons/Mono/SearchIcon.vue';
+import SettingsIcon from '@icons/Mono/SettingsIcon.vue';
+import SortHorizontalIcon from '@icons/Mono/SortHorizontalIcon.vue';
+import SortDownIcon from '@icons/Mono/SortDownIcon.vue';
+import SortUpIcon from '@icons/Mono/SortUpIcon.vue';
+import SortVerticalIcon from '@icons/Mono/SortVerticalIcon.vue';
+import LineIcon from '@icons/Mono/LineIcon.vue';
+import TableIcon from '@icons/Mono/TableIcon.vue';
+import TrashIcon from '@icons/Mono/TrashIcon.vue';
+import UserIcon from '@icons/Mono/UserIcon.vue';
+import ArrowsVerticalIcon from '@icons/Mono/ArrowsVerticalIcon.vue';
+import TriangleIcon from '@icons/Mono/TriangleIcon.vue';
+import Playground from '@/Playground.vue';
+import ArrowShortDownIcon from '@icons/Mono/ArrowDownShortIcon.vue';
+import StarIcon from '@icons/Mono/StarIcon.vue';
+import StarFilledIcon from '@icons/Mono/StarFilledIcon.vue';
+import ArrowLeftShortIcon from '@icons/Mono/ArrowLeftShortIcon.vue';
+import ArrowDoubleLeftShortIcon from '@icons/Mono/ArrowDoubleLeftShortIcon.vue';
+import ArrowRightShortIcon from '@icons/Mono/ArrowRightShortIcon.vue';
+import ArrowDoubleRightShortIcon from '@icons/Mono/ArrowDoubleRightShortIcon.vue';
+import CornerLeftBottomIcon from '@icons/Mono/CornerLeftBottomIcon.vue';
+import CornerLeftTopIcon from '@icons/Mono/CornerLeftTopIcon.vue';
+import CornerRightBottomIcon from '@icons/Mono/CornerRightBottomIcon.vue';
+import CornerRightTopIcon from '@icons/Mono/CornerRightTopIcon.vue';
+import InfoIcon from '@icons/Mono/InfoIcon.vue';
+import WarningIcon from '@icons/Mono/WarningIcon.vue';
+import CrossRoundIcon from '@icons/Mono/CrossRoundIcon.vue';
+// Components
+import Button from '@components/Button/Button.vue';
+import Carousel from '@components/Carousel/Carousel.vue';
+import Checkbox from '@components/Checkbox/Checkbox.vue';
+import ColorPicker from '@components/ColorPicker/ColorPicker.vue';
+import Divider from '@components/Divider/Divider.vue';
+import Drawer from '@components/Drawer/Drawer.vue';
+import InputDiv from '@components/InputDiv/InputDiv.vue';
+import Knob from '@components/Knob/Knob.vue';
+import MenuDial from '@components/MenuDial/MenuDial.vue';
+import Modal from '@components/Modal/Modal.vue';
+import Paginator from '@components/Paginator/Paginator.vue';
+import Popup from '@components/Popup/Popup.vue';
+import ProgressBar from '@components/ProgressBar/ProgressBar.vue';
+import Rating from '@components/Rating/Rating.vue';
+import Select from '@components/Select/Select.vue';
+import SelectButton from '@components/SelectButton/SelectButton.vue';
+import Slider from '@components/Slider/Slider.vue';
+import Table from '@components/Table/Table.vue';
+import Tag from '@components/Tag/Tag.vue';
+import Toast from '@components/Toast/Toast.vue';
+import ToggleSwitch from '@components/ToggleSwitch/ToggleSwitch.vue';
+import TreeList from '@components/TreeList/TreeList.vue';
+
+export {
+  Age18Icon,
+  AlarmIcon,
+  AirplaneIcon,
+  ArrowLeftIcon,
+  ArrowForwardIcon,
+  ArchiveIcon,
+  AnchorLinkIcon,
+  AnchorIcon,
+  AlignRightIcon,
+  AlignLeftIcon,
+  AlignCenterIcon,
+  ArrowRightIcon,
+  AtIcon,
+  AwardIcon,
+  BackspaceIcon,
+  BadgeIcon,
+  BallFootballIcon,
+  BallIcon,
+  BanknoteIcon,
+  BellIcon,
+  BellOffIcon,
+  BoxIcon,
+  BrightnessIcon,
+  BulbIcon,
+  CalculatorIcon,
+  CalendarIcon,
+  CalendarAddIcon,
+  CalendarRemoveIcon,
+  CardsIcon,
+  ChartLineIcon,
+  ChartPieIcon,
+  ChatIcon,
+  ChatWritingIcon,
+  CheckMarkIcon,
+  ChemistryFlaskIcon,
+  CodeIcon,
+  CoinsIcon,
+  ColorPaletteIcon,
+  CompassIcon,
+  ConstructionWorkerIcon,
+  ContactsIcon,
+  CropIcon,
+  CrossRoundFilledIcon,
+  CrossIcon,
+  CubeIcon,
+  CupIcon,
+  CursorIcon,
+  LineDashedIcon,
+  LineDiagonalIcon,
+  DiamondIcon,
+  DiaryIcon,
+  DiceIcon,
+  DigIcon,
+  DislikeIcon,
+  DisplayIcon,
+  DocumentIcon,
+  DollarIcon,
+  DotsHorizontalIcon,
+  DotsVerticalIcon,
+  LineDottedIcon,
+  DownloadIcon,
+  DropIcon,
+  DumbbelIcon,
+  EarthIcon,
+  EditIcon,
+  EyeIcon,
+  EncyclopediaIcon,
+  ExitIcon,
+  FeedbackIcon,
+  FilterIcon,
+  FingerprintIcon,
+  FireIcon,
+  FlagIcon,
+  FlashIcon,
+  FlashlightIcon,
+  FolderLockIcon,
+  FrameIcon,
+  FullScreenIcon,
+  GameControllerIcon,
+  GiftIcon,
+  GlassesIcon,
+  HamburgerIcon,
+  HandIcon,
+  HomeIcon,
+  ImageEditIcon,
+  ImageIcon,
+  MoveIcon,
+  DocumentAddIcon,
+  DocumentDeleteIcon,
+  DocumentEditIcon,
+  ParagraphIcon,
+  PhoneHandsetIcon,
+  PlusCircleIcon,
+  PlusIcon,
+  PointerIcon,
+  SaveIcon,
+  SearchIcon,
+  SettingsIcon,
+  SortHorizontalIcon,
+  SortDownIcon,
+  SortUpIcon,
+  SortVerticalIcon,
+  LineIcon,
+  TableIcon,
+  TrashIcon,
+  UserIcon,
+  ArrowsVerticalIcon,
+  TriangleIcon,
+  Playground,
+  ArrowShortDownIcon,
+  StarIcon,
+  StarFilledIcon,
+  ArrowLeftShortIcon,
+  ArrowDoubleLeftShortIcon,
+  ArrowRightShortIcon,
+  ArrowDoubleRightShortIcon,
+  CornerLeftBottomIcon,
+  CornerLeftTopIcon,
+  CornerRightBottomIcon,
+  CornerRightTopIcon,
+  InfoIcon,
+  WarningIcon,
+  CrossRoundIcon,
+  Button,
+  Carousel,
+  Checkbox,
+  ColorPicker,
+  Divider,
+  Drawer,
+  InputDiv,
+  Knob,
+  MenuDial,
+  Modal,
+  Paginator,
+  Popup,
+  ProgressBar,
+  Rating,
+  Select,
+  SelectButton,
+  Slider,
+  Table,
+  Tag,
+  Toast,
+  ToggleSwitch,
+  TreeList,
+};
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,28 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import { resolve, dirname } from 'node:path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, './src/index.ts'),
+      name: 'FeatherUI',
+      fileName: 'featherui',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+    // outDir: path.join(__dirname, 'dist'),
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
